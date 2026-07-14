25541ebdfac59432ace08b1538376de09cdc03df
Windy <windynapitupulu3108@gmail.com>
2026-07-14 12:18:19 +0700

    Initial Commit

---
+++ Chromi/ChromiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChromiApp.swift
+//  Chromi
+//
+//  Created by Windy Claudia Napitupulu on 14/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ChromiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
